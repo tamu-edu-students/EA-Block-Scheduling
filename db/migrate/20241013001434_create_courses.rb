a5815558e88a675aa9898e7197831930cb151a5b
Aaron Jones <45701418+AaronHJones@users.noreply.github.com>
2024-10-13 13:33:59 -0500

    Add seed data and course model rspec test

--- db/migrate/20241013001434_create_courses.rb
+++ db/migrate/20241013001434_create_courses.rb
@@ -1,27 +1,29 @@
 class CreateCourses < ActiveRecord::Migration[7.2]
   def change
-    create_table :courses do |t|
-      t.string :term
-      t.text :dept_code
-      t.string :course_id
-      t.string :sec_coreq_secs
-      t.string :syn
-      t.string :sec_name
-      t.string :short_title
-      t.integer :im
-      t.string :building
-      t.string :room
-      t.string :days
-      t.time :start_time
-      t.time :end_time
-      t.string :fac_id
-      t.string :faculty_name
-      t.integer :crs_capacity
-      t.integer :sec_cap
-      t.integer :student_count
-      t.string :notes
+    unless table_exists? :courses
+      create_table :courses do |t|
+        t.string :term
+        t.text :dept_code
+        t.string :course_id
+        t.string :sec_coreq_secs
+        t.string :syn
+        t.string :sec_name
+        t.string :short_title
+        t.integer :im
+        t.string :building
+        t.string :room
+        t.string :days
+        t.time :start_time
+        t.time :end_time
+        t.string :fac_id
+        t.string :faculty_name
+        t.integer :crs_capacity
+        t.integer :sec_cap
+        t.integer :student_count
+        t.string :notes
 
-      t.timestamps
+        t.timestamps
+      end
     end
   end
 end
