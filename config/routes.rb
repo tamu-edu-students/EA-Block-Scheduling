a3c5e9788aefdec51361c44087cf58b1a115215d
Ryann Lu <lu.ryann.2n@tamu.edu>
2024-10-18 22:12:46 -0500

    add Auth0 controller and routes

--- config/routes.rb
+++ config/routes.rb
@@ -17,4 +17,9 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+
+  # For Auth0 redirects
+  get '/auth/auth0/callback' => 'auth0#callback'
+  get '/auth/failure' => 'auth0#failure'
+  get '/auth/logout' => 'auth0#logout'
 end
