a59107931b32abb0c82ca414c8ea6201ed3fa6e2
Adithi <adithis_197@tamu.edu>
2024-11-01 08:11:59 -0500

    SSO Integration

--- config/routes.rb
+++ config/routes.rb
@@ -1,26 +1,35 @@
 Rails.application.routes.draw do
-  # showing classes to choose
-  resources :courses
-  resources :schedules, only: [:index, :show]
-  # shows all courses
-  get "/courses", to: "courses#index"
-  # shows courses to choose from
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
+  root "welcome#index", to: "welcome#index", as: "welcome"
+
+  # Health check route
   get "up" => "rails/health#show", as: :rails_health_check
-  # Render dynamic PWA files from app/views/pwa/*
+
+  # PWA routes
   get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
+  get "/auth/failure", to: "sessions#failure"  # Optional, for handling failed authentication
+  get "/auth/google_oauth2/callback", to: "sessions#omniauth"
+  get "admin/dashboard", to: "admin#dashboard", as: :admin_dashboard
+  get "dashboard", to: "students#dashboard", as: :students_dashboard
 
-  get "sessions/new"
-  get "sessions/create"
-  get "sessions/sso_new"
-  get "sessions/sso_create"
-  get "users/new"
-  get "users/create"
-  get "home/index"
-  get "generate-schedule", to: "schedules#generate_schedule"
+  # Course and schedule routes
   resources :courses
+  resources :schedules, only: [:index, :show] do
+    collection do
+      get "generate_schedule", to: "schedules#generate_schedule"
+    end
+  end
+
+  # User and session routes
+  resources :users, only: [:new, :create, :show]
+  resource :session, only: [:new, :create, :destroy] do
+    collection do
+      get "sso_new"
+      get "sso_create"
+      get "/logout", to: "sessions#logout", as: "logout"
+    end
+  end
+
+  # Excel file routes
   resources :excel_files
-  root "excel_files#index"
 end
