ae9692e702e2c2dde1b55af63ff6988e449f08f1
Adithi <adithis_197@tamu.edu>
2024-11-06 15:42:26 -0600

    Add more rspec tests

--- spec/support/authentication_helper.rb
+++ spec/support/authentication_helper.rb
@@ -3,6 +3,34 @@ module AuthenticationHelper
     user ||= FactoryBot.create(:user) # Create or use a default user for login
     post login_path, params: { session: { email: user.email, password: user.password } }
   end
+
+  def login_as_admin_user
+    OmniAuth.config.test_mode = true
+    OmniAuth.config.mock_auth[:google_oauth2] = OmniAuth::AuthHash.new({
+      provider: 'google_oauth2',
+      uid: '12345',
+      info: { email: 'adminuser@example.com', name: 'Admin User' }
+    })
+
+    visit '/auth/google_oauth2/callback'  # Simulates OmniAuth callback
+  end
+
+  def login_as_student_user
+    OmniAuth.config.test_mode = true
+    OmniAuth.config.mock_auth[:google_oauth2] = OmniAuth::AuthHash.new({
+      provider: 'google_oauth2',
+      uid: '67890',
+      info: { email: 'studentuser@example.com', name: 'Student User' },
+      role: 'student'
+    })
+
+    # Simulate OmniAuth callback
+    visit '/auth/google_oauth2/callback'
+  end
+
+  def reset_sessions!
+    Capybara.reset_sessions!
+  end
 end
 
 RSpec.configure do |config|
