dc0ff805d233ee2c0434c9ccd5fe7feadcfe0870
Aaron Jones <45701418+AaronHJones@users.noreply.github.com>
2024-11-08 21:08:27 -0600

    Merge in from origin
    # Conflicts:
    #	coverage/.resultset.json
    #	coverage/index.html
    #	db/development.sqlite3
    #	db/test.sqlite3

--- config/routes.rb
+++ config/routes.rb
@@ -36,7 +36,6 @@ Rails.application.routes.draw do
 
   # Excel file routes
   resources :excel_files
-  root "excel_files#index"
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
