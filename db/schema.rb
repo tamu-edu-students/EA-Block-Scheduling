e7b2d8bc69b9075b1825081bff5c32aa31279865
Aaron Jones <45701418+AaronHJones@users.noreply.github.com>
2024-11-14 23:26:32 -0600

    Add user_role and role to db and seed file

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_11_11_201457) do
+ActiveRecord::Schema[7.2].define(version: 2024_11_15_030129) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -74,6 +74,19 @@ ActiveRecord::Schema[7.2].define(version: 2024_11_11_201457) do
 
   create_table "excel_files", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "roles", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "user_roles", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "role_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
