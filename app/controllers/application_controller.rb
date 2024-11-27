103b02b0605ebaca8ad2261511b4927f75e7224b
Aaron Jones <45701418+AaronHJones@users.noreply.github.com>
2024-11-26 19:34:46 -0600

    Complete reverting back to functional commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,12 +8,6 @@ class ApplicationController < ActionController::Base
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
 
-  def require_role(role_name)
-    unless current_user&.has_role?(role_name)
-      redirect_to pages_path, alert: "You don't have the required role (#{role_name}) to access this page."
-    end
-  end
-
   def require_admin
     unless current_user&.admin?
       redirect_to pages_path, alert: "You don't have access to this page."
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,21 +2,24 @@ class User < ApplicationRecord
   has_many :user_roles
   has_many :roles, through: :user_roles
   validates :email, presence: true
+  after_initialize :set_default_role, if: :new_record?
 
   def admin?
-    has_role?("admin")
-  end
-
-  def student?
-    has_role?("student")
+    role == "admin"
   end
 
   def has_role?(role_name)
     roles.any? { |role| role.name == role_name }
   end
 
+  # Method to check if the user is a student
+  def student?
+    role == "student"
+  end
+
   private
+
   def set_default_role
-    self.roles << Role.find_by(name: "student") if roles.empty?
+    self.role ||= "student"
   end
 end
