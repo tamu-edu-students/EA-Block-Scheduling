1a13814437d68bc47c1d37c395cf2e6fec20c098
Aaron Jones <aaron.h.jones@outlook.com>
2024-11-29 18:28:59 -0600

    Add small correction to courses and footer

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,6 @@ class ApplicationController < ActionController::Base
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
 
-
   def require_admin
     unless current_user&.admin?
       redirect_to login_path, alert: "You don't have access to this page."
--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,8 +1,8 @@
 class CoursesController < ApplicationController
   include CoursesHelper
+  before_action :require_admin
   # before_action :authenticate_user!
   # before_action :admin_only, only: [:upload, :new, :create, :edit, :update, :destroy]
-  before_action :set_course, only: %i[ show edit update destroy ]
 
   # GET /courses or /courses.json
   def index
@@ -10,7 +10,6 @@ class CoursesController < ApplicationController
     @prerequisites = {}
     @corequisites = {}
     @categories = {}
-
 
     @courses.each do |course|
       if course.prerequisites.present?
@@ -24,7 +23,7 @@ class CoursesController < ApplicationController
 
   # GET /courses/1 or /courses/1.json
   def show
-    @prerequisites = @course.prerequisites&.split(", ")&.map(&:strip)
+    @course = Course.where(id: params[:id]).first
   end
 
   # POST /courses or /courses.json
@@ -38,6 +37,10 @@ class CoursesController < ApplicationController
       flash.now[:alert] = @course.errors.full_messages.join(", ")
       render :new, status: :unprocessable_entity
     end
+  end
+
+  def edit
+    @course = Course.where(id: params[:id]).first
   end
 
   # PATCH/PUT /courses/1 or /courses/1.json
@@ -70,76 +73,38 @@ class CoursesController < ApplicationController
     @courses = Course.where(as_id: as_id)
   end
 
-  # Gets the course code from the sec_name (e.g. MATH-2414 from MATH-2414-007)
-  # def extract_base_code(sec_name)
-  #   standardized = sec_name.gsub(' ', '-')
-  #   parts = standardized.split('-')
-  #   "#{parts[0]}-#{parts[1]}"
-  # end
-
   private
-
-  # Define prereqs and coreqs for listed courses for populating columns during creation
-  # def corequisites
-  #   {
-  #     'ENGR 102' => %w[MATH-2412 MATH-2413],
-  #     'ENGR 216' => ['PHYS 2425'],
-  #     'ENGR 217' => ['PHYS 2426']
-  #   }
-  # end
-  # def prerequisites
-  #   {
-  #     'MATH-2413' => ['MATH-2412'],
-  #     'MATH-2414' => ['MATH-2413'],
-  #     'MATH-2415' => ['MATH-2414'],
-  #     'MATH-2420' => ['MATH-2415'],
-  #     'ENGR-216' => %w[ENGR-102 MATH-2413],
-  #     'ENGR-217' => %w[ENGR-216 PHYS-2425 MATH-2414],
-  #     'CHEM-1312' => ['CHEM-1309'],
-  #     'CHEM-1112' => ['CHEM-1309'],
-  #     'PHYS-2425' => ['MATH-2413'],
-  #     'PHYS-2426' => %w[MATH-2414 PHYS-2425]
-  #   }
-  # end
-
   # Use callbacks to share common setup or constraints between actions.
   def set_course
     @course = Course.find(params[:id])
   end
 
-    # Only allow a list of trusted parameters through.
-    def course_params
-      params.require(:course).permit(
-        :term,
-        :dept_code,
-        :course_id,
-        :sec_coreq_secs,
-        :syn,
-        :sec_name,
-        :short_title,
-        :im,
-        :building,
-        :room,
-        :days,
-        :start_time,
-        :end_time,
-        :fac_id,
-        :faculty_name,
-        :crs_capacity,
-        :sec_cap,
-        :student_count,
-        :notes,
-        :as_id,
-        :prerequisites,
-        :corequisites,
-        :category
-      )
-    end
-
-  # def admin_only
-  #   unless current_user.admin?
-  #     flash[:alert] = "User is not authorized to perform this action."
-  #     redirect_to courses_path
-  #   end
-  # end
+  # Only allow a list of trusted parameters through.
+  def course_params
+    params.require(:course).permit(
+      :term,
+      :dept_code,
+      :course_id,
+      :sec_coreq_secs,
+      :syn,
+      :sec_name,
+      :short_title,
+      :im,
+      :building,
+      :room,
+      :days,
+      :start_time,
+      :end_time,
+      :fac_id,
+      :faculty_name,
+      :crs_capacity,
+      :sec_cap,
+      :student_count,
+      :notes,
+      :as_id,
+      :prerequisites,
+      :corequisites,
+      :category
+    )
+  end
 end
